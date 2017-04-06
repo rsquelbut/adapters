39cde1fb56ca74d1d46e617da0e85cb4037e75a8
rsquelbut <java4job@gmail.com>
2017-04-06 15:16:37 +0200

    ajoute les annotations @Override manquantes

--- src/main/java/fr/squelbut/adapter/bulk/ListAdapter.java
+++ src/main/java/fr/squelbut/adapter/bulk/ListAdapter.java
@@ -3,7 +3,7 @@ package fr.squelbut.adapter.bulk;
 import fr.squelbut.adapter.Adapter;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -13,7 +13,7 @@ import java.util.function.Supplier;
  */
 public class ListAdapter<I, O> implements Adapter<I, O> {
 
-    private final List<Adapter<I, O>> transformers = new ArrayList<>();
+    private final Collection<Adapter<I, O>> transformers = new ArrayList<>();
 
 
     public static <I, O> ListAdapter<I, O> with(Class<I> input, Class<O> output) {
@@ -26,11 +26,15 @@ public class ListAdapter<I, O> implements Adapter<I, O> {
         return output;
     }
 
+
+    @Override
     public ListAdapter<I, O> adapt(Adapter<I, O> then) {
         transformers.add(then);
         return this;
     }
 
+
+    @Override
     public <V> ListAdapter<I, O> adapt(Function<I, V> getter, BiConsumer<O, V> setter) {
         return adapt((I i, O o) -> {
             setter.accept(o, getter.apply(i));
@@ -38,9 +42,9 @@ public class ListAdapter<I, O> implements Adapter<I, O> {
         });
     }
 
+    @Override
     public <V> ListAdapter<I, O> adapt(Supplier<V> getter, BiConsumer<O, V> setter) {
         Function<I, V> function = (I input) -> getter.get();
         return adapt(function, setter);
     }
-
 }
